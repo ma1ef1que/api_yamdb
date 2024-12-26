f8266d658cd1bd2d319413983ace6bc684b7f9ae
Kirill Pelevin <i@pelevin-k.ru>
2024-12-26 16:03:14 +0200

    first review fix users logic

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -31,3 +31,44 @@ class AuthorOrReadOnly(permissions.BasePermission):
             or getattr(request.user, 'is_admin', False)
             or getattr(request.user, 'is_moderator', False)
         )
+
+
+class IsAdmin(permissions.BasePermission):
+    """
+    Разрешение для администратора или суперпользователя.
+    """
+
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return request.user.is_superuser or request.user.is_admin
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_authenticated:
+            return request.user.is_superuser or request.user.is_admin
+        return False
+
+
+class IsModerator(permissions.BasePermission):
+    """
+    Разрешение для модераторов проекта.
+    """
+
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated and request.user.role == 'moderator')
+
+    def has_object_permission(self, request, view, obj):
+        return self.has_permission(request, view)
+
+
+class IsUser(permissions.BasePermission):
+    """
+    Разрешение для авторизованных пользователей.
+    """
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        return self.has_permission(request, view)
